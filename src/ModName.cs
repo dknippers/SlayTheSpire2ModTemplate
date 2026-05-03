7b4ad6ae8914aa727565828db9ce61f25f0b77ab
Daniël Knippers <dknippers@gmail.com>
2026-05-03 14:11:13 +0200

    Add sample patch that modifies Ironclad's Strike damage to 10

--- src/ModName.cs
+++ src/ModName.cs
@@ -1,3 +1,4 @@
+using HarmonyLib;
 using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Modding;
 
@@ -8,6 +9,9 @@ public static class ModName
 {
     public static void Init()
     {
+        var harmony = new Harmony(nameof(ModName));
+        harmony.PatchAll(typeof(ModName).Assembly);
+
         Log.Info("ModName loaded successfully");
     }
 }
---
+++ src/Patches/ModifyIroncladStrikeDamage.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace ModName.Patches;
+
+[HarmonyPatch]
+public static class ModifyIroncladStrikeDamage
+{
+    [HarmonyPatch(typeof(StrikeIronclad), MethodType.Constructor)]
+    [HarmonyPostfix]
+    public static void StrikeIronclad_Constructor(StrikeIronclad __instance)
+    {
+        // Modify damage: 6 -> 10
+        __instance.DynamicVars.Damage.BaseValue = 10m;
+    }
+}
